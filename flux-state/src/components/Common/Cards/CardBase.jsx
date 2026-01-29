8baac413baedb7fd5ea90760c21183770276ee98
Itamar Henriques <itamar.henriques.rn@gmail.com>
2026-01-29 17:53:14 -0300

    Ajuste dos Cards
    Fiz o ajuste para que os Cards sejam apresentados como no design.

--- flux-state/src/components/Common/Cards/CardBase.jsx
+++ flux-state/src/components/Common/Cards/CardBase.jsx
@@ -1,7 +1,7 @@
 function CardBase({ w, h, titleCard }) {
     return (
         <>
-            <div style={{ width: w, height: h }} className="relative bg-[#1D1D1D] rounded-2xl p-2 pl-2.5 text-left m-4">
+            <div style={{ width: w, height: h }} className="relative bg-[#1D1D1D] rounded-2xl p-2 pl-2.5 text-left">
                 <p>{titleCard}</p>
             </div>
         </>
--- flux-state/src/components/Layout/Main.jsx
+++ flux-state/src/components/Layout/Main.jsx
@@ -4,25 +4,28 @@ import StackedCard from "../Common/Cards/StackedCard"
 function Main() {
     return (
         <>
-            <main className="bg-amber-800 w-full h-full ml-10 flex justify-self-auto flex-wrap">
-                <>
-                    <CardBase w='245px' h='153px' titleCard='CPU Load / Temp.' />
-                    <CardBase w='245px' h='153px' titleCard='GPU Load / Temp.' />
-                    <CardBase w='245px' h='153px' titleCard='System Temp.' />
-                </>
-                <StackedCard />
+            <main className=" w-full h-full ml-10 flex">
 
-                <>
-                    <CardBase w='528px' h='195px' titleCard='Resource Timeline (60s)' />
-                    <CardBase w='245px' h='195px' titleCard='System Health Score' />
-                </>
+                <div className="relative w-[60%] col-span-4 flex flex-row flex-wrap gap-3 justify-between ml-6 mr-8">
 
-                <CardBase w='258px' h='273px' titleCard='Alerts' />
 
-                <>
-                    <CardBase w='386px' h='157px' titleCard='Uptime' />
-                    <CardBase w='386px' h='157px' titleCard='Active Processes' />
-                </>
+                    <CardBase titleCard='CPU Load / Temp.' w='245px' h='153px' />
+                    <CardBase titleCard='GPU Load / Temp.' w='245px' h='153px' className='' />
+                    <CardBase titleCard='System Temp.' w='245px' h='153px' />
+
+                    <CardBase titleCard='Resource Timeline (60s)' w='528px' h='195px' />
+                    <CardBase titleCard='System Health Score' w='245px' h='195px' />
+
+                    <CardBase titleCard='Uptime' w='386px' h='157px' />
+                    <CardBase titleCard='Active Processes' w='386px' h='157px' />
+
+                </div>
+                <div className=" mt-10 flex flex-col justify-between">
+                    <StackedCard />
+                    <CardBase titleCard='Alerts' w='258px' h='273px' />
+                </div>
+
+
             </main>
         </>
     )
